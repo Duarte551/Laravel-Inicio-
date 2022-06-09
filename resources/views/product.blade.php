1c8a95307a85285b1a8f435022431347a15d0ddb
Duarte <joaovictordfs551@gmail.com>
2022-06-08 22:10:15 -0300

    Testes na aplicação

---
+++ resources/views/product.blade.php
@@ -0,0 +1,2 @@
+<h1>Tela de Produtos</h1>
+<a href="/">Clique para voltar pra tela inicial</a>
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+
+    $nome = 'Duarte';
+    $idade = 18;
+    $profissao = 'Programador';
+    $array = [1,2,3,4,5];
+    $nomes = ['Pedro', 'Thiago', 'João', 'Matheus'];
+
+    return view('welcome' , 
+    ['nome' => $nome, 
+    'idade' => $idade, 
+    'profissao' => $profissao, 
+    'array' => $array,
+    'nomes' => $nomes
+]);
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/product', function () {
+    return view('product');
+});
